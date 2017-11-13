7cbde768a690f53c32d1dc81936f12449824907e
vekkiokonio <luigivigneri89@gmail.com>
2017-11-13 11:58:35 +0100

    Bug fixing - random coefficients for utility function
    A first complete version of the simulator for EGD with fixed step size.

--- Network+Utility+Maximization.py
+++ Network+Utility+Maximization.py
@@ -3,7 +3,7 @@
 
 # # Simulator for NUM problems
 
-# In[12]:
+# In[1]:
 
 
 import numpy as np
@@ -13,7 +13,16 @@ import pdb
 
 # Function definition
 
-# In[13]:
+# In[2]:
+
+
+def generate_coeff(source):
+    """ Generate random coefficients for the utility function """    
+    
+    return np.random.rand(source)
+
+
+# In[3]:
 
 
 def generate_link(link):
@@ -23,7 +32,7 @@ def generate_link(link):
     return scale * np.random.rand(link)
 
 
-# In[14]:
+# In[4]:
 
 
 def generate_path(source, path, link):
@@ -41,7 +50,7 @@ def generate_path(source, path, link):
 #     x^{(0)}_{s,p}=\min_{l\in L}\left\{ \frac{0,9\cdot c_l}{\sum_{s,p}\mathbf{1}_{s,p\ni l}}\right\}, \qquad \forall s\in S, p\in P_s
 # \end{equation}
 
-# In[15]:
+# In[5]:
 
 
 def initial_solution(x, cl):
@@ -55,16 +64,16 @@ def initial_solution(x, cl):
 #     \sum_{s\in S}a_s\cdot\log\left(\sum_{p\in P_s}x_{s,p}\right), \qquad a_s\in [0, 1].
 # \end{equation}
 
-# In[16]:
+# In[6]:
 
 
-def compute_utility(x, source):
+def compute_utility(x, source, coeff):
     """ Compute the utility at the current iteration """
     
-    return sum(np.random.rand(source) * np.log(np.sum(np.max(x, axis=2), axis=1)))
+    return sum(coeff * np.log(np.sum(np.max(x, axis=2), axis=1)))
 
 
-# In[17]:
+# In[7]:
 
 
 def backtracking_stepsize():
@@ -73,7 +82,7 @@ def backtracking_stepsize():
     return
 
 
-# In[18]:
+# In[8]:
 
 
 def plot_utility(y):
@@ -86,9 +95,17 @@ def plot_utility(y):
     plt.show()
 
 
-# We use the barrier function:
+# We use the logarithmic barrier function
+# \begin{equation}
+#     \quad B_l({y}) = \begin{cases} -\log(c_l - y), & \mbox{if } {y}< c_l, \\ \infty, & \mbox{if } {y}\geq c_l. \end{cases}
+# \end{equation}
+# 
+# The gradient step for the exponentiated gradient descent is
+# \begin{equation}
+#     x_{s,p}^{(k+1)} = x_{s,p}^{(k)}\cdot \exp \left\{\eta^{(k)}\cdot \left[U_s'\left(x^{(k)}_{s,p}\right) - \sum_{l:l\in s,p}\frac{\mu^{(k)}}{\sum x^{(k)}_{s,p} - c_l}\right]\right\}
+# \end{equation}
 
-# In[19]:
+# In[9]:
 
 
 def gradient_step(old_x, source, link, path, step_size, barrier, cl):
@@ -112,52 +129,57 @@ def gradient_step(old_x, source, link, path, step_size, barrier, cl):
 
 # Variable initialization
 
-# In[20]:
+# In[26]:
 
 
 # Max number of iterations
-max_iter =  10000
+max_iter =  100
 
 # Number of OD pairs
-source = 5
+source = 4
 
 # Number of paths per OD pair
-path = 5 * np.ones(source, dtype='int32')
+path = 2 * np.ones(source, dtype='int32')
 
 # Number of links
-link = 10
-
-# Fixed step size
-step_size = 0.01
-barrier = 0.01
-
-# Output array of utilities
-utility = np.zeros((max_iter + 1))
+link = 5
 
 
-# In[21]:
+# In[11]:
 
+
+# Define utility
+coeff = np.random.rand(source)
 
 # Generate link capacity
 cl = generate_link(link)
 
 # Generate paths
-x = generate_path(source, path, link)
+x0 = generate_path(source, path, link)
 
 
 # Body of the simulator
 
-# In[22]:
+# In[27]:
+
+
+# Fixed step size
+step_size = 10
+barrier = 10
+
+
+# In[28]:
 
 
 # Initial feasible solution
-x = initial_solution(x, cl)
+x = initial_solution(x0, cl)
+utility = np.zeros((max_iter + 1))
 
-utility[0] = (compute_utility(x, source))
+utility[0] = compute_utility(x, source, coeff)
 for i in range(max_iter):
 #    step_size = backtracking_stepsize()    Uncomment to use backtracking line search
     x = gradient_step(x, source, link, path, step_size, barrier, cl)
-    utility[i] = (compute_utility(x, source))
+    utility[i + 1] = compute_utility(x, source, coeff)
         
 # Plot utility
 plot_utility(utility)
